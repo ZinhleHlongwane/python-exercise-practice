d560ce29f2fca062e54aa8a9fd4c1772210c7ef0
Zinhle Hlongwane <zinhlehlongwani56@gmail.com>
2026-01-08 01:00:17 +0200

    Add problem docstrings to all Python exercise files

---
+++ file_io/bar_graph.py
@@ -0,0 +1,15 @@
+def bar_graph(filename: str) -> str:
+    """
+    Creates a text-based bar graph of class averages.
+
+    Each line in the file represents a class.
+    Each number represents a student's mark.
+
+    One '*' represents 10%.
+
+    Args:
+        filename (str): name of the file containing marks
+
+    Returns:
+        str: formatted bar graph
+    """
